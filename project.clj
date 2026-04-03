2c25061933fb7c878955b150a4524a25a979fea9
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-04 08:33:45 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject jank-myhtml-test "0.1-SNAPSHOT"
+  :license {:name "MIT"
+            :url "https://mit-license.org"}
+  :dependencies []
+  :plugins [[org.jank-lang/lein-jank "0.6"]]
+  :middleware [leiningen.jank/middleware]
+  :jank {:include-dirs ["include"]
+         :library-dirs ["lib"]
+         :linked-libraries ["myhtml"]}
+  :main jank-myhtml-test.main
+  :profiles {:base {:jank {:output-dir "target/debug"
+                           :optimization-level 0}}
+             :release {:jank {:output-dir "target/release"
+                              :optimization-level 2}}})
